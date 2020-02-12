c899d6f179e3ab4ae890b28c194bc885e5eece2e
Reuben Remy <reubenremy@gmail.com>
2020-02-12 14:00:03 -0500

    Added Members to App Component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Member from '../Members/Member';
 
 const App = () => (
     <div className="App">
-        <Member />
-        <Member />
-        <Member />
+        <Member name='Reu' age='27'>Father of the Family</Member>
+        <Member name='Kay' age='24'>Mother of the Family</Member>
+        <Member name='Rylee' age='3'>Oldest Child in the Family</Member>
     </div>
 );
 
