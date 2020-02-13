25cdb3acf3e83c1f31a91ae972736cd2163f3b43
Reuben Remy <reubenremy@gmail.com>
2020-02-12 22:46:16 -0500

    Cleaned up Syntax in App.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -7,10 +7,19 @@ class App extends Component {
     state = {
         Members: [
             //Members wil be an Array [] of objects {}
-            { name: 'Reu', age:31 },
-            { name: 'Kay', age:28 },
-            { name: 'Rye', age: 4 },
-            { name: 'Vava', age: 1 }
+            { 
+                name: 'Reu', 
+                age:31 
+            },{ 
+                name: 'Kay', 
+                age:28 
+            },{ 
+                name: 'Rye', 
+                age: 4 
+            },{ 
+                name: 'Vava', 
+                age: 1 
+            }
         ]
     }
 
@@ -20,10 +29,19 @@ class App extends Component {
         //React doesn't recognize direct mutations to state without Component's Inherit method called "setState"
         this.setState({
             Members: [
-                { name: 'Reuben', age:31 },
-                { name: 'Kethny', age:28 },
-                { name: 'Rylee', age: 4 },
-                { name: 'Reva', age: 1 }
+                { 
+                    name: 'Reuben', 
+                    age:31 
+                },{ 
+                    name: 'Kethny', 
+                    age:28 
+                },{ 
+                    name: 'Rylee', 
+                    age: 4 
+                },{ 
+                    name: 'Reva', 
+                    age: 1 
+                }
             ]
         });
     
@@ -32,10 +50,22 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <Member name={ this.state.Members[0].name } age={ this.state.Members[0].age }>Father of the Family</Member>
-                <Member name={ this.state.Members[1].name } age={ this.state.Members[1].age }>Mother of the Family</Member>
-                <Member name={ this.state.Members[2].name } age={ this.state.Members[2].age }>Oldest Child of the Family</Member>
-                <Member name={ this.state.Members[3].name } age={ this.state.Members[3].age }>Youngest Child of the Family</Member>
+                <Member 
+                    name={ this.state.Members[0].name } 
+                    age={ this.state.Members[0].age }
+                    >Father of the Family</Member>
+                <Member 
+                    name={ this.state.Members[1].name } 
+                    age={ this.state.Members[1].age }
+                    >Mother of the Family</Member>
+                <Member 
+                    name={ this.state.Members[2].name } 
+                    age={ this.state.Members[2].age }
+                    >Oldest Child of the Family</Member>
+                <Member 
+                    name={ this.state.Members[3].name } 
+                    age={ this.state.Members[3].age }
+                    >Youngest Child of the Family</Member>
                 {/* 
                     - Remember that "This" refers to the class
                     - Also you want to pass the method or "executable property" as a reference
