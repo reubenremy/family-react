7ac2eb5e7cd88d9ed77c1447ac3db6fd21eb09b5
Reuben Remy <reubenremy@gmail.com>
2020-02-14 23:31:38 -0500

    Code Dismantle

--- src/Components/Members/Member.js
+++ src/Components/Members/Member.js
@@ -4,14 +4,7 @@ import './Members.css';
 const Member = props => (
     <div className="members" onClick={ props.click }>
         <h1>Name: { props.name } <br/>Age: { props.age }</h1>
-        <p>Description:<br/>{ props.children }</p>..
-        <input 
-            type='text' 
-            placeholder= "New Name"
-            onChange={ props.changed } 
-            value={ props.value }
-        />
-    </div>
-)
+        <p>Description:<br/>{ props.children }</p>
+       
 
 export default Member;
